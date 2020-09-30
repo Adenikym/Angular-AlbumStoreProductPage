e180499284be8804d14549f4200852c168f04125
Adenikym <mm5adeniky@gmail.com>
2020-09-30 12:47:55 +0100

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 
-import { Observable } from 'rxjs/Observable';
-import  'rxjs/add/operator/map'
+import  'rxjs/add/operator/map';
+
+
 @Injectable()
+
+
 export class ProductService {
-id;
+
    private _albumUrl='../assets/album.json';
 
-  constructor(private _http: Http) { }
+  constructor(private _http: Http
+    ) { };
 
   
 
-  getAlbum (id:number) {
-  return this._http.get(this._albumUrl).map((response:Response)=>{
-    return response.json()
+  getAlbum(id : number) {
+  return this._http.get(this._albumUrl).map((response) =>{
+     response.json()
 
   })
 
