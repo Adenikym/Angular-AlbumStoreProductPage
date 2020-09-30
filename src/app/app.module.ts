c4f72cfdd0b391ff6db2aa18b9029eddc980525c
Adenikym <mm5adeniky@gmail.com>
2020-09-30 14:05:26 +0100

    product tracklisting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-import {ProductService} from './product.service'
+import {ProductService} from './product.service';
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
-    ProductDescriptionComponent
+    ProductDescriptionComponent,
+    ProductTracklistingComponent
     
     
   ],
