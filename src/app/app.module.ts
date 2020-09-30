71914273190c402309798e6ec3ef3f8fe0425136
Adenikym <mm5adeniky@gmail.com>
2020-09-30 12:32:12 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-import {ProductService} from '../app/product.service'
+import {ProductService} from './product.service'
 
 
 @NgModule({
