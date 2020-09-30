3b2ab5eb7639ec4f4acd05353e7e95659f19a90e
Adenikym <mm5adeniky@gmail.com>
2020-09-30 09:38:41 +0100

    new

---
+++ src/app/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import {Response} from '@angular/http';
+@Injectable()
+export class ProductService {
+
+  _albumUrl='../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum (id:number) {
+  
+  }
+  
+}
+
