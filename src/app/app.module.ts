2f1cedd1168512236ef82b7ef51a16fb43abbb1f
Adenikym <mm5adeniky@gmail.com>
2020-09-30 15:33:37 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import {RouterModule} from '@angular/router';
+import Routes from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
@@ -26,7 +27,17 @@ import { ProductListComponent } from './product-list/product-list.component'
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    Routes
+  ],
+
+  appRoutes:Routes[
+    {key:ProductService,
+    component:ProductListComponent},
+    {key:product/:id,
+      component:ProductPageComponent
+    }
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
